60d5e81063b76576ea5fa694f8d97292ccd39658
Richard Good <richard.good@ed.ac.uk>
2016-11-28 14:00:47 +0000

    API027 Add subscription message with correct encryption/decryption components.

--- src/main/java/uk/ac/ed/notify/entity/SubscriptionMessage.java
+++ src/main/java/uk/ac/ed/notify/entity/SubscriptionMessage.java
@@ -1,5 +1,18 @@
 package uk.ac.ed.notify.entity;
 
+import org.bouncycastle.jce.ECNamedCurveTable;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
+import org.bouncycastle.jce.spec.ECPublicKeySpec;
+import org.bouncycastle.math.ec.ECPoint;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+
 /**
  * Created by rgood on 25/11/2016.
  */
@@ -11,13 +24,7 @@ public class SubscriptionMessage {
 
     private String key;
 
-    public String getKey() {
-        return key;
-    }
-
-    public void setKey(String key) {
-        this.key = key;
-    }
+    private String auth;
 
     public String getEndPoint() {
         return endPoint;
@@ -33,5 +40,47 @@ public class SubscriptionMessage {
 
     public void setUun(String uun) {
         this.uun = uun;
+    }
+
+    public void setAuth(String auth) {
+        this.auth = auth;
+    }
+
+    public String getAuth() {
+        return auth;
+    }
+
+    /**
+     * Returns the base64 encoded auth string as a byte[]
+     */
+    public byte[] getAuthAsBytes() {
+        return Base64.getDecoder().decode(getAuth());
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    /**
+     * Returns the base64 encoded public key string as a byte[]
+     */
+    public byte[] getKeyAsBytes() {
+        return Base64.getDecoder().decode(getKey());
+    }
+
+    /**
+     * Returns the base64 encoded public key as a PublicKey object
+     */
+    public PublicKey getUserPublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchProviderException {
+        KeyFactory kf = KeyFactory.getInstance("ECDH", BouncyCastleProvider.PROVIDER_NAME);
+        ECNamedCurveParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("secp256r1");
+        ECPoint point = ecSpec.getCurve().decodePoint(getKeyAsBytes());
+        ECPublicKeySpec pubSpec = new ECPublicKeySpec(point, ecSpec);
+
+        return kf.generatePublic(pubSpec);
     }
 }
